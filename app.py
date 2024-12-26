7d2c2b0acc439eba69e73b79a3244fdbfb873947
Ronald Marques <ronaldpmarques01@gmail.com>
2024-12-26 16:15:46 -0300

    Implementação da rota de criação de pagamento & Criação do modelo Pix

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from datetime import datetime, timedelta
 from repository.database import db
 from db_models.payment import Payment
+from payments.pix import Pix
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///databse.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = "SECRET_KEY_WEBSOCKET"
+db.init_app(app)
+
 
 @app.route("/payments/pix", methods=["POST"])
 def create_payment_pix():
-    return jsonify({"message": "O pagamento foi criado!"})
+    data = request.get_json()
+
+    if 'value' not in data:
+        return jsonify({"message": "Valor invalido"}), 400
+
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data["value"], expiration_date=expiration_date)
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+    new_payment.qr_code = data_payment_pix["qr_code_path"]
+
+    db.session.add(new_payment)
+    db.session.commit()
+    return jsonify({"message": "O pagamento foi criado!", "payment": new_payment.to_dict()})
 
 
 @app.route("/payments/pix/confirmation", methods=["POST"])
